ea4deee37ae20f346947a7f2eec10e2b5fcbe4f0
User <emmanuelbakare@yahoo.com>
2021-05-02 22:02:00 +0100

    Added new structure to Estate model to handle m2m rel

--- estate/models.py
+++ estate/models.py
@@ -31,17 +31,23 @@ class Estate(models.Model):
     state_region = models.CharField(_("state or region"), max_length=100)
     country = models.CharField(_("country"), max_length=100)
     # address= models.OneToOneField(EstateAddress, blank=True, null=True, on_delete=models.CASCADE)
-    admin=models.ManyToManyField(User, verbose_name=_("admin"))
+    # admin=models.ManyToManyField(User, verbose_name=_("admin"))
     comment=models.TextField(_("comment"), null=True, blank=True)
+    created_date= models.DateTimeField(_("Created Date"),auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_date']
 
     def __str__(self):
         return  f'{self.name} -  {self.city}, {self.country}'
 
-# class EstateAdmin(models.Model):
-#     user = models.ManyToManyField(User, verbose_name=_("user"))
-#     estate=models.ForeignKey(Estate ,verbose_name=_("Estate"), on_delete=models.CASCADE)
-#     # estate=models.ManyToManyField(Estate, verbose_name=_("Estate"))
-#     perms = models.IntegerField(_("permission"), null=True, blank=True)
+class EstateAdmin(models.Model):
+    user = models.ForeignKey(User, verbose_name=_("User"), on_delete=models.CASCADE)
+    estate=models.ForeignKey(Estate, verbose_name=_("Estate"), on_delete=models.CASCADE, related_name="admin")
+    perms = models.IntegerField(_("permission"), null=True, blank=True)
+
+    class Meta:
+        unique_together=['user','estate']
 
 
 
